599c51aac211768ee61409637842a2351cc9afcd
Antonio <atorralba96@gmail.com>
2018-10-08 09:56:40 +0200

    Remotos

--- common/models/juego.js
+++ common/models/juego.js
@@ -1,5 +1,27 @@
 'use strict';
 
-module.exports = function(Juego) {
+module.exports = function (Juego) {
+    /**
+     * Da nombre
+     * @param {Function(Error, string)} callback
+     */
+
+    Juego.getName = function (callback) {
+        var nombre;
+        // TODO
+        callback(null, nombre);
+    };
+
+    /**
+     * da nombre
+     * @param {Function(Error, string)} callback
+     */
+
+    Juego.prototype.getNameInstancia = function (callback) {
+        //var nombre;
+        // TODO
+        callback(null, this.nombre);
+    };
+
 
 };
